6ab7e3f2abfdf258d76c262239c2586c8b20f2bf
Saqib <iamsaqib24@gmail.com>
2022-08-08 15:30:37 +0530

    used for loop and some methods

--- js1/Programs/countVowels.js
+++ js1/Programs/countVowels.js
@@ -1,24 +1,47 @@
 // to count vowels present in a string
 
-function countVowels(word){
+// program using while loop
+// function countVowels(word){
+//     let vowelCount = 0;
+//     let index = 0;
+//     while(index < word.length){
+//         let letter = word[index];
+//         if ( letter === "a" ||
+//             letter === "e" ||
+//             letter === "i" ||
+//             letter === "o" ||
+//             letter === "u" ) {
+//                 vowelCount += 1;
+//             }
+//             index++;
+//     }
+//     return vowelCount;
+// }
 
-    let vowelCount = 0;
-    let index = 0;
-    while(index < word.length){
-        let letter = word[index];
-        if( letter === "a" ||
-            letter === "e" ||
-            letter === "i" ||
-            letter === "o" ||
-            letter === "u" ){
-                vowelCount += 1;
+// console.log(countVowels("saqib"));
+// console.log(countVowels("learning"));
+// console.log(countVowels("javascript"));
+// console.log(countVowels("bootcamp"));
+
+
+// program using for loop and some pre-defined methods.
+
+function countVowels(word){
+    let vowelsCount = 0;
+    let string = word.toString();
+
+    for(let i = 0; i < string.length; i++){
+        if (string.charAt(i) === "a" ||
+            string.charAt(i) === "e" ||
+            string.charAt(i) === "i" ||
+            string.charAt(i) === "o" ||
+            string.charAt(i) === "u" ) {
+                vowelsCount = vowelsCount + 1;
             }
-            index++;
     }
-    return vowelCount;
+    return vowelsCount;
 }
 
-console.log(countVowels("saqib"));
+console.log(countVowels("bootcamp"));
 console.log(countVowels("learning"));
 console.log(countVowels("javascript"));
-console.log(countVowels("bootcamp"));
